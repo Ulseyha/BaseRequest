854883e9a0ad3d39280365d5ce1781c3bff90733
Seyha <ulseyha@gmail.com>
2019-03-12 16:38:13 +0700

    [update] name

--- app/src/main/java/com/pathmazing/hr/mvvm/navigator/LoginNavigator.kt
+++ app/src/main/java/com/pathmazing/hr/mvvm/navigator/LoginNavigator.kt
@@ -4,5 +4,6 @@ import com.pathmazing.baserequest.mvvm.navigator.BaseNavigator
 import com.pathmazing.hr.mvvm.model.LoginResponse
 
 interface LoginNavigator : BaseNavigator {
+
     fun onLoginSucceed(loginResponse: LoginResponse)
 }
